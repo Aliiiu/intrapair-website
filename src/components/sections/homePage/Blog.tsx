af13d14b40f2f77b59fcb7f598a51864e61f14f9
Boluwatife-AJB <56480003+Boluwatife-AJB@users.noreply.github.com>
2023-01-29 21:20:16 +0100

    blog card animation initialization

--- src/components/sections/homePage/Blog.tsx
+++ src/components/sections/homePage/Blog.tsx
@@ -8,6 +8,10 @@ const Blog = () => {
   const [blogs, setBlogs] = useState<any>([]);
   const [onFocus, setonFocus] = useState<boolean>(false);
 
+  // const handleArrow = (id: any) => {
+  //   console.log(id);
+  // };
+
   useEffect(() => {
     const fetchBlog = async () => {
       const response = await fetch(
@@ -53,11 +57,13 @@ const Blog = () => {
               // Check if blog is sucessfully fetched from medium json
               blogs.status === 'ok' ? (
                 <div className="xl:flex-nowrap flex-wrap flex  gap-[36px] justify-between w-full">
-                  {blogs.items.slice(0, 3).map((item: any, id: number) => {
+                  {blogs.items.slice(0, 3).map((item: any, id: any) => {
                     return (
                       <div
                         key={id}
-                        onClick={(id) => console.log('I was clicked!', id)}
+                        onClick={() => {
+                          setonFocus(true);
+                        }}
                         className="flex flex-col gap-1 max-w-[350px] sm:max-w-[300px] xl:max-w-[400px] mx-auto items-center"
                         style={{
                           boxShadow:
